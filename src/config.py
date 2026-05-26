054f40a3e8475de4de65a592799b3ac46d970226
Adewale Franklin Hassan <122757918+adewalefranklin@users.noreply.github.com>
2026-05-26 09:53:44 +0200

    Exceptions, config & logger implemented

---
+++ src/config.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from src.exceptions import ConfigError
+
+load_dotenv()
+
+class Config:
+    required_variables = ['AGSI_API_KEY', 'AGSI_BASE_URL']
+
+    @classmethod
+    def get(cls, var_name):
+        value = os.getenv(var_name)
+        if not value:
+            raise ConfigError(f"Variable not found: {var_name}")
+        return value
+    
+    @classmethod
+    def validate(cls):
+        for var in cls.required_variables:
+            cls.get(var)
+
+                
+    
