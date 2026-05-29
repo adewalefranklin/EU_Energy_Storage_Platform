8e2fbf1d47c4ad57f29db8a139f7c289b15fcc8d
Adewale Franklin Hassan <122757918+adewalefranklin@users.noreply.github.com>
2026-05-29 09:15:40 +0200

    Snowflake models modified

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -7,16 +7,10 @@ def test_extract_data_success(mocker):
     fake_response.raise_for_status.return_value = None
     fake_response.json.return_value = {"data": "test_data"}
 
-    mocker.patch(
-        "eu_energy_pipeline.extract.requests.get",
-        return_value=fake_response
-    )
+    mocker.patch("eu_energy_pipeline.extract.requests.get", return_value=fake_response)
 
     extractor = Extractor()
 
-    result = extractor.fetch_data(
-        "test_endpoint",
-        {"param1": "value1"}
-    )
+    result = extractor.fetch_data("test_endpoint", {"param1": "value1"})
 
     assert result == {"data": "test_data"}
